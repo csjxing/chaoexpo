f29508d89e9f69e3ffc45f4a723036d3c180f50b
csjxing <csjxing@163.com>
2013-05-23 20:19:29 +0800

    skip task

--- code/bundle/rpm/app-config.sh
+++ code/bundle/rpm/app-config.sh
@@ -4,4 +4,4 @@ echo "run app-config.sh target = $TARGET"
 
 auto_config $TARGET/chaoexpo.bundle.war-1.0-SNAPSHOT.war
 auto_config $TARGET/web-deploy.jar
-auto_config $TARGET/chaoexpo.task-uber.jar
+##auto_config $TARGET/chaoexpo.task-uber.jar

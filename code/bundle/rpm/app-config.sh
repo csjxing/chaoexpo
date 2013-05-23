5c55b4485a48fe36bbb0bf0c5ad846fcdaae0866
csjxing <csjxing@163.com>
2013-05-24 00:59:52 +0800

    trace

--- code/bundle/rpm/app-config.sh
+++ code/bundle/rpm/app-config.sh
@@ -2,6 +2,9 @@
 
 echo "run app-config.sh target = $TARGET"
 
+echo "start config war ..."
 auto_config $TARGET/chaoexpo.bundle.war-1.0-SNAPSHOT.war
+echo "start config web-deploy ..."
 auto_config $TARGET/web-deploy.jar
+echo "start config task-uber ..."
 auto_config $TARGET/chaoexpo.task-uber.jar
